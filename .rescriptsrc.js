9d91f42598d8d8dee4b641a1f7dc0e9713428068
do-kevin <do.kevin.vo@gmail.com>
2020-07-10 05:03:57 -0700

    Feat: Uses personal starter & adds Grommet

---
+++ .rescriptsrc.js
@@ -0,0 +1,43 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const logConfig = (config) => {
+  return config;
+};
+
+logConfig.isMiddleware = true;
+
+module.exports = [
+  'env',
+  ['use-postcss-config'],
+  {
+    webpack: (config) => {
+      if (config.mode === 'production') {
+        config.plugins = [
+          ...config.plugins,
+          // new BundleAnalyzerPlugin({
+          //   analyzerMode: 'static',
+          //   reportFilename: 'analyzed-bundle.html',
+          // }),
+        ];
+      }
+
+      if (config.mode === 'development') {
+        config.plugins = [...config.plugins, new ReactRefreshWebpackPlugin()];
+      }
+
+      config.plugins = [
+        ...config.plugins,
+        new LodashModuleReplacementPlugin({
+          shorthands: true,
+          collections: true,
+          paths: true,
+          cloning: true,
+        }),
+      ];
+
+      return config;
+    },
+  },
+  logConfig,
+];
