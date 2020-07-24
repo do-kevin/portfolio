4d1aeb04f58399993323c75c5b9d49814b1779de
do-kevin <do.kevin.vo@gmail.com>
2020-07-23 19:12:57 -0700

    ⚙️ Adds constants & styled folders

---
+++ src/constants/breakpoints.ts
@@ -0,0 +1,3 @@
+export const bp1 = '48em'; // 768px
+export const bp2 = '160em'; // 2560px
+export const bp3 = '240em'; // 3840px
---
+++ src/constants/mediaQueries.ts
@@ -0,0 +1,7 @@
+import { bp1, bp2, bp3 } from 'constants/breakpoints';
+
+export const mq1 = `only screen and (min-width: ${bp1})`;
+export const mq2 = `only screen and (min-width: ${bp2})`;
+export const mq3 = `only screen and (min-width: ${bp3})`;
+
+export const mq4 = `only screen and (max-width: ${bp1})`;
